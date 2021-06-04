2ca5ecc729ef2c0401b57f837bbc4d70ac000006
Tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2021-06-04 16:16:59 +0900

    aggregate_failures の設定を追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,14 +33,15 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true unless meta.has_key?(:aggregate_failures)
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
