e941fec347edfec151b4b9782a2a727f9c86a9d6
Tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2021-06-06 14:42:07 +0900

    必要な controller の追加

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Admin::ApplicationController < ActionController::Base
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get "posts/index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :users
+    resources :posts
+  end
+
+  resources :posts, only: [:index]
 end
