fb2005535bffa3c63627793ced8f3c5e1c968a19
Tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2021-06-14 22:19:36 +0900

    model の共通メソッドを追加

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,16 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.human_attribute_enum_value(attr_name, value)
+    human_attribute_name("#{attr_name}.#{value}")
+  end
+
+  def human_attribute_enum(attr_name)
+    self.class.human_attribute_enum_value(attr_name, self[attr_name]) if self[attr_name]
+  end
+
+  def self.enum_options_for_select(attr_name)
+    send(attr_name.to_s.pluralize).map {|k, _| [human_attribute_enum_value(attr_name, k), k] }.to_h
+  end
 end
