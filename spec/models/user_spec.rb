ca606ce5506ea39305893594e26d8da2e64cf600
Tomokazu <atlof_fed_1win6_tomo@yahoo.co.jp>
2021-06-17 17:47:58 +0900

    User に uid を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,7 @@
 #  email      :string           not null
 #  name       :string           not null
 #  profile    :string
+#  uid        :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
